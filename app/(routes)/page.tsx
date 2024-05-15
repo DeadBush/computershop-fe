9df745b7b312d8835840eb17cb19a6eb08c1fb34
DeadBush <nguyenbakyanh1@gmail.com>
2024-05-16 00:37:38 +0700

    Commit 1 16/5/24

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import  Container  from "@/components/container";
+
+const HomePage = () =>{
+    return (
+        <>
+        <Container className="px-4 md:px-12">
+            <section></section>
+        </Container>
+        </>
+    )
+}
+
+export default HomePage;
---
+++ components/main-nav.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+interface MainNavProps extends React.HTMLAttributes<HTMLElement>{
+    scrolled : boolean
+}
+
+const MainNav = ({className, scrolled,...props} : MainNavProps) =>{
+    
+    const pathname = usePathname()
+    const params = useParams()
+
+    const routes = [
+        {
+            href : "/",
+            label : "Home",
+            active : pathname === "/"
+        },
+        {
+            href : "/menu",
+            label : "Menu",
+            active : pathname === "/menu"
+        },
+        {
+            href : "/orders",
+            label : "Orders",
+            active : pathname === "/orders"
+        },
+        {
+            href : "/about",
+            label : "About",
+            active : pathname === "/about"
+        },
+        {
+            href : "/contact",
+            label : "Contact",
+            active : pathname === "/contact"
+        },
+    ]
+
+    return <div className="ml-auto">
+        <nav className={cn("flex items-center space-x-4 lg:space-x-12 pl-6", className)}>{routes.map(routes =>(
+            <Link 
+            href={routes.href}
+            key={routes.href}
+            className={cn("text-base font-medium transition-colors hover:text-primary", 
+            routes.active 
+                ? `${
+                    scrolled 
+                        ? "text-hero font-bold" 
+                        : "text-black dark: text-white"
+                    }` 
+                    : `${scrolled ? "text-black" : "text-white"}`
+                )}
+            >
+                {routes.label}
+            </Link>
+        ))}</nav>
+    </div>
+}
+
+export default MainNav;
