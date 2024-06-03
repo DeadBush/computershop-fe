4aa894a3cc2a938ea938a9d41ad613cd6b4837a2
DeadBush <nguyenbakyanh1@gmail.com>
2024-06-03 14:09:36 +0700

    123123

--- app/(routes)/orders/page.tsx
+++ app/(routes)/orders/page.tsx
@@ -1,5 +1,32 @@
-const OrdersPage = () =>{
-    return <div></div>
+import getOrders from "@/actions/get-orders";
+import Box from "@/components/ui/box";
+import Container from "@/components/ui/container";
+import { auth } from "@clerk/nextjs/server";
+import { ChevronRight, Home } from "lucide-react";
+import Link from "next/link";
+import PageContent from "../menu/components/page-content";
+
+const OrdersPage =  async() =>{
+
+    const orders = await getOrders();
+    const{userId}= auth();
+
+    const formattedOrders = orders.filter(item => item.userId === userId);
+    return <Container className="px-4 md:px-12 my-12 bg-white py-12 min-h-[80vh]">
+        <Box className="text-neutral-700 text-sm items-center">
+            <Link href="/" className="flex items-center gap-2">
+                <Home className="w-5 h-5"/> Trang chủ
+            </Link>
+
+            <ChevronRight className="w-5 h-5 text-muted-foreground"/>
+            <p className="flex items-center gap-2 text-muted-foreground">
+                Đơn hàng của tôi
+            </p>
+        </Box>
+        <h2 className="my-4 text-xl font-semibold text-neutral-700"> Đơn hàng của tôi</h2>
+
+        <PageContent/>
+    </Container>
 };
 
 export default OrdersPage;
